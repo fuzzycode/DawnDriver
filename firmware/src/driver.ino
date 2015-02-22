3376d45d40492cecc51432d9752c9ff4d4330bd7
Björn Larsson <b.e.larsson@gmail.com>
2015-02-22 14:09:42 +0100

    Updated structure of the microcontroler firmware

--- firmware/src/driver.ino
+++ firmware/src/driver.ino
@@ -27,7 +27,7 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 
 #include <Servo.h>
-#include "Encoder.h"
+#include "lib/Encoder.h"
 
 //------------------------------------------------------------------------------
 const uint8_t VERSION_MAJOR = 0;
