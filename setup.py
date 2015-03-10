9a7150e274192f659dd4885c5121bfd57586d7bb
Björn Larsson <b.e.larsson@gmail.com>
2015-03-10 21:21:22 +0100

    Fleshed out the setup file with more accurate information

---
+++ setup.py
@@ -0,0 +1,60 @@
+#! /usr/bin/env python
+
+# Copyright (c) 2014, Dawn Robotics Ltd
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+# 1. Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+
+# 3. Neither the name of the Dawn Robotics Ltd nor the names of its contributors
+# may be used to endorse or promote products derived from this software without
+# specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+
+"""
+
+from distutils.core import setup
+
+__version__=(1,0,0)
+
+
+if __name__ == "__main__":
+
+    setup(name="DawnDriver",
+          version=str(__version__),
+          author="Alan Broun(Dawn Robotics)",
+          author_email='abroun@alanbroun.net',
+          maintainer="Bjoern Larsson",
+          maintainer_email="develop@bjornlarsson.net",
+          description='A Dagu Mini Driver',
+          long_description=__docs__,
+          url="",
+          keywords="robotics firmware",
+          install_requires=['ino'],
+          classifiers=[f.strip() for f in """
+          Development Status :: 4 - Beta
+          Intended Audience :: Developers
+          Operating System :: POSIX :: Linux
+          Programming Language :: Python
+          Topic :: Software Development :: Libraries :: Python Modules
+          Topic :: Utilities""".splitlines() if f.strip()]
+    )
